a6ff744f546d5c00dddd7c5264317b657ac07cea
Montijn <ms.bennik@student.avans.nl>
2023-03-21 13:06:41 +0100

    Initial Commit

---
+++ AlwaysNote/AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysNoteApp.swift
+//  AlwaysNote
+//
+//  Created by Montijn Bennik on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
